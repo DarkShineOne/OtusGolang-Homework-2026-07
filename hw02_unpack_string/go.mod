2c76f2ca4ae37d48716dc7027c7a518df72a1c4a
DarkShineOne <51234139+DarkShineOne@users.noreply.github.com>
2026-08-16 20:57:32 +1000

    homework: hw02_unpack_string

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/darkshineone/otusgolang-homework-2026-07/hw02_unpack_string
 
 go 1.23
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,56 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+const backslash = rune(0x5C)
+
+func isASCIIDigit(r rune) bool {
+	return r >= '0' && r <= '9'
+}
+
+func Unpack(s string) (string, error) {
+	runes := []rune(s)
+
+	var sb strings.Builder
+
+	for i := 0; i < len(runes); {
+		var ch rune
+
+		switch {
+		case runes[i] == backslash:
+			if i+1 >= len(runes) {
+				return "", ErrInvalidString
+			}
+
+			next := runes[i+1]
+
+			if !isASCIIDigit(next) && next != backslash {
+				return "", ErrInvalidString
+			}
+
+			ch = next
+			i += 2
+
+		case isASCIIDigit(runes[i]):
+			return "", ErrInvalidString
+
+		default:
+			ch = runes[i]
+			i++
+		}
+
+		count := 1
+		if i < len(runes) && isASCIIDigit(runes[i]) {
+			count = int(runes[i] - '0')
+			i++
+		}
+
+		sb.WriteString(strings.Repeat(string(ch), count))
+	}
+
+	return sb.String(), nil
 }
