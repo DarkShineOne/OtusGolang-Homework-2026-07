33486e4b6a753530cfca443050db43c9839ad5f2
DarkShineOne <51234139+DarkShineOne@users.noreply.github.com>
2026-08-17 18:47:18 +1000

    homework: hw02_unpack_string (#2)
    * homework: hw02_unpack_string

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,15 +18,23 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: "αβγ2", expected: "αβγγ"},
+		{input: "🎉2🎊3", expected: "🎉🎉🎊🎊🎊"},
+		{input: "a", expected: "a"},
+		{input: "a9", expected: "aaaaaaaaa"},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: "的3", expected: "的的的"},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `的\3`, expected: `的3`},
+		{input: `a\1`, expected: `a1`},
+		{input: `\5a`, expected: `5a`},
+		{input: `a\5`, expected: `a5`},
+		{input: "a٥a", expected: "a٥a"},
 	}
 
 	for _, tc := range tests {
-		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -36,9 +44,17 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{
+		"3abc",
+		"45",
+		"aaa10b",
+		"qw\\ne",
+		"a\\",
+		"\\",
+		"\\a",
+		"a\\b",
+	}
 	for _, tc := range invalidStrings {
-		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
